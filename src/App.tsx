72ed92c47419e2448ed9df9d4c2bc73d16d757cd
Jaffer Wehliye <jafferwehliye@eduroam-campus-10-36-29-189.campus-dynamic.uwaterloo.ca>
2025-10-29 14:21:41 -0400

    Added Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import Browse from "./pages/Browse";
 import Submit from "./pages/Submit";
@@ -30,6 +31,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <VercelAnalytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
